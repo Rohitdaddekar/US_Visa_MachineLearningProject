5d5ef90638a904dee65673c0835990c742eeed8b
Rohit <rohitdaddekar24@gmail.com>
2024-07-22 16:49:35 +0530

    requirements.txt updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="rohit",
+    author_email="rohitdaddekar24@gmail.com",
+    packages=find_packages()
+)
